e4c2002a4cc77a0aba803653676eb233cb615eb5
Marco Lecona <blazedosan02@gmail.com>
2020-09-04 12:24:39 -0500

    Method returnUnits added

--- numberToSpeech/src/numbertospeech/NumberToSpeech.java
+++ numberToSpeech/src/numbertospeech/NumberToSpeech.java
@@ -1,21 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package numbertospeech;
 
-/**
- *
- * @author Mark
- */
+import java.util.Scanner;
+
 public class NumberToSpeech {
 
-    /**
-     * @param args the command line arguments
-     */
+    String unitArray[] = new String[]{"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        NumberToSpeech numbertospeech = new NumberToSpeech();
+
+        Scanner scan = new Scanner(System.in);
+
+        String input;
+
+        System.out.println("Enter A Number");
+        
+        input = scan.next();
+        
+        numbertospeech.returnUnits(input);
+
     }
-    
+
+    public String returnUnits(String input) {
+
+        switch (input) {
+
+            case "0":
+
+                input = unitArray[0];
+
+                System.out.println(input);
+
+                break;
+
+            case "1":
+
+                input = unitArray[1];
+
+                System.out.println(input);
+
+                break;
+
+            case "2":
+
+                input = unitArray[2];
+
+                System.out.println(input);
+
+                break;
+            case "3":
+
+                input = unitArray[3];
+
+                System.out.println(input);
+
+                break;
+            case "4":
+
+                input = unitArray[4];
+
+                System.out.println(input);
+
+                break;
+            case "5":
+
+                input = unitArray[5];
+
+                System.out.println(input);
+
+                break;
+            case "6":
+
+                input = unitArray[6];
+
+                System.out.println(input);
+
+                break;
+            case "7":
+
+                input = unitArray[7];
+
+                System.out.println(input);
+
+                break;
+            case "8":
+
+                input = unitArray[8];
+
+                System.out.println(input);
+
+                break;
+            case "9":
+
+                input = unitArray[9];
+
+                System.out.println(input);
+
+                break;
+
+        }
+
+        return input;
+    }
+
 }
